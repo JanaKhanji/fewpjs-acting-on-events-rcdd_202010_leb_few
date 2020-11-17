d46b27b437ca27d58742c8e7f777bf62a0a4aa0f
Jana Khanji <jana.khanji@ieee.org>
2020-11-17 07:28:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,12 @@ document.addEventListener("keydown", function(e) {
   }
 });
 
-
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
 
