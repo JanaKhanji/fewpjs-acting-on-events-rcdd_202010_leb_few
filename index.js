9ee2f92fb4d65499b5bee150278b6607fff8ef76
Jana Khanji <jana.khanji@ieee.org>
2020-11-17 07:28:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 dodger.style.bottom = "100px";
 
-
+document.addEventListener("keydown", function(e) {
+  
+});
 
