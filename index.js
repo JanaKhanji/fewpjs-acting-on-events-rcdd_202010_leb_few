789e19b52b45ee70dd79ed22a333967d438e79ce
Jana Khanji <jana.khanji@ieee.org>
2020-11-17 07:30:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ document.addEventListener("keydown", function(e) {
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
- 
+
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
   }
 }
-
